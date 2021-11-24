41ff5635f22d9d0539f2739d8bcbeb016c0d95c3
katpachecob <kathepachecobaca@gmail.com>
2021-11-24 15:34:39 -0500

    holi

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello app!</Text>
+    <View style={styles.container} >
+      <LinearGradient 
+      colors={[ "purple",'rgba(72, 154, 199, 0.59)']} 
+      start={{
+        x: 2,
+        y: 0.5
+      }}
+    
+      style={styles.container} >
+
+      <Text>Hello gonoo!</Text>
+      </LinearGradient>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +25,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
+    height: '100%',
   },
 });
