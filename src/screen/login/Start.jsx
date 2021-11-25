b14555412f44d3c67a264658954ef07e29414d17
MartinMza <medizam.99@gmail.com>
2021-11-25 17:57:27 -0300

    new view Login

---
+++ src/screen/login/Start.jsx
@@ -0,0 +1,12 @@
+import react from "React";
+import {
+  Button,
+  Image,
+  View,
+  TouchableOpacity,
+  Text,
+  TextInput,
+  StyleSheet,
+} from "react-native";
+import logo from "../../../assets/logo.png";
+import Gradient from "../../components/gradient/Gradient";
