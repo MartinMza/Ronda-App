e290017b09ecb194eb358dfa13d304d1f11bbd6f
MatiDifi <difioremati@gmail.com>
2021-12-08 12:46:58 -0300

    fixing posts and calendar Co-authored-by: Andy Tieftrunk <AndyTieftrunk@users.noreply.github.com> Co-authored-by: Julio Cesar Sanchez P. <juliuscaesar169@users.noreply.github.com>

--- api/routes/calendar.js
+++ api/routes/calendar.js
@@ -3,4 +3,10 @@ const GoogleCalendarAPI = require('../services/calendar');
 
 router.post('/set', GoogleCalendarAPI.setEvent);
 
+router.get('/get', GoogleCalendarAPI.getEvents);
+router.get('/getcalendar', GoogleCalendarAPI.getCalendars)
+
+router.get('/getone/:eventId', GoogleCalendarAPI.getSingleEvent);
+
+router.delete('/delete/:eventId', GoogleCalendarAPI.deleteEvent);
 module.exports = router;
